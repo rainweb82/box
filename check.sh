e7d06f2b6569d095ab8f42513fbe1e5e9f47ddc2
rainweb82 <105602643+rainweb82@users.noreply.github.com>
2023-10-21 05:12:42 -0500

    Add files via upload

---
+++ check.sh
@@ -0,0 +1,3 @@
+
+
+curl https://www.konami.cc/bbs/ --cookie "WAjr_2132_auth=8f67EeQOWychoFxzx5BNMhCOpbT5URIB%2BnZgpEFuJqGF58LmusyMFr%2BcReEU72gJw64H3dlvxTeT%2F8nPq%2F0cut7AWcjE;&WAjr_2132_saltkey=hnR9FgQR;"
