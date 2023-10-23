54080cfdbb15ed02ce2e6d152fdb41d8c95d016b
rainweb82 <105602643+rainweb82@users.noreply.github.com>
2023-10-23 11:15:47 -0500

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,4 +2,4 @@ url="https://www.konami.cc/bbs/"
 cookie="WAjr_2132_auth=8f67EeQOWychoFxzx5BNMhCOpbT5URIB%2BnZgpEFuJqGF58LmusyMFr%2BcReEU72gJw64H3dlvxTeT%2F8nPq%2F0cut7AWcjE;WAjr_2132_saltkey=hnR9FgQR;"
 
 strA="`curl --retry 3 --retry-max-time 30 -L -s "$url" --cookie "$cookie"`"
-print $strA
+printf $strA
