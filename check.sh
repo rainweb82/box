a813c7a604deb21c44ca4c161cf2df49d91ec4e6
rainweb82 <105602643+rainweb82@users.noreply.github.com>
2024-11-02 15:52:41 +0800

    Update and rename check.sh to known_domains.txt

--- check.sh
+++
@@ -1,7 +0,0 @@
-url="https://www.konami.cc/bbs/"
-cookie="WAjr_2132_auth=8f67EeQOWychoFxzx5BNMhCOpbT5URIB%2BnZgpEFuJqGF58LmusyMFr%2BcReEU72gJw64H3dlvxTeT%2F8nPq%2F0cut7AWcjE;WAjr_2132_saltkey=hnR9FgQR;"
-
-strA="`curl --retry 3 --retry-max-time 30 -L -s "$url" --cookie "$cookie"`"
-printf $strA
-
-
